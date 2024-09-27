79503b5e07837ca8487613032b2ba260110ac892
Oliver-Nec <oliver.nec@gmail.com>
2024-09-27 09:25:58 +0200

    Basic structure

---
+++ src/app/o-mne/page.tsx
@@ -0,0 +1,14 @@
+// src/app/o-mne/page.tsx
+
+import Typography from '@mui/material/Typography';
+
+export const metadata = { title: "O mne | ZoskaSnap" };
+
+export default function AboutMe() {
+
+  return (
+
+      <Typography> O mne </Typography>
+
+  );
+}
---
+++ src/app/profil/[id]/page.tsx
@@ -0,0 +1,14 @@
+// src/app/profil/[id]/page.tsx
+
+import Typography from '@mui/material/Typography';
+
+export const metadata = { title: "Detail profilu | ZoskaSnap" };
+
+export default function ProfileDetail() {
+
+  return (
+
+      <Typography> Detail profilu </Typography>
+
+  );
+}
