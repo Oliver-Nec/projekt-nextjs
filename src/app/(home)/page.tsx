b778acf1aaac9ffba46ef4ee9e320190dc4775f5
Oliver-Nec <oliver.nec@gmail.com>
2024-10-25 09:01:11 +0200

    build after next auth working

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,16 @@
-// src/app/(home)/page.tsx
+// src/app/page.tsx
 
-import Typography from '@mui/material/Typography';
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/authOptions";
+import AuthHomeView from "../../sections/AuthHomeView";
+import NonAuthHomeView from "@/sections/NonAuthHomeView";
 
-export const metadata = { title: "Domov | ZoskaSnap" };
+export const metadata = { title: "Domov | ZoškaSnap" };
 
-export default function Home() {
+export default async function HomePage() {
+  // Fetch session on the server
+  const session = await getServerSession(authOptions);
 
-  return (
-
-      <Typography> Domovska stranka </Typography>
-
-  );
+  // Conditionally render authenticated or non-authenticated home view
+  return session ? <AuthHomeView session={session} /> : <NonAuthHomeView />;
 }
-
-
---
+++ src/components/AuthProvider.tsx
@@ -0,0 +1,13 @@
+// src/components/AuthProvider.tsx
+"use client";
+
+import React, { ReactNode } from 'react'
+import { SessionProvider } from 'next-auth/react'
+
+const AuthProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <SessionProvider>{children}</SessionProvider>
+  )
+}
+
+export default AuthProvider
